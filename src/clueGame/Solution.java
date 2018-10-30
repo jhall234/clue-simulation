1eecef7b2e7c96444c9b9b05cfa7437a245ae7f4
Joshua Hallinan <jhallinan@mymail.mines.edu>
2018-10-30 14:51:44 -0600

    Added Solution class

---
+++ src/clueGame/Solution.java
@@ -0,0 +1,17 @@
+package clueGame;
+
+public class Solution {
+	public String person;
+	public String room;
+	public String weapon;
+	
+	public Solution() {
+		
+	}
+	
+	public Solution(String person, String room, String weapon) {
+		this.person = person;
+		this.room = room;
+		this.weapon = weapon;
+	}
+}
