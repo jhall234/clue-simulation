2c1ceee9ede41f0d17a392ca40a5ce00dca50d36
Joshua Hallinan <jhallinan@mymail.mines.edu>
2018-10-30 14:43:09 -0600

    Added Player, ComputerPlayer and Human Player classes

---
+++ src/clueGame/ComputerPlayer.java
@@ -0,0 +1,22 @@
+package clueGame;
+
+import java.util.*;
+
+public class ComputerPlayer extends Player {
+	public ComputerPlayer() {
+		
+	}
+	
+	public BoardCell pickLocation(Set<BoardCell> targets) {
+		BoardCell board_cell = new BoardCell();
+		return board_cell;
+	}
+	
+	public void makeAccusation() {
+		
+	}
+	
+	public void createSuggestion() {
+		
+	}
+}
---
+++ src/clueGame/HumanPlayer.java
@@ -0,0 +1,22 @@
+package clueGame;
+
+import java.util.*;
+
+public class HumanPlayer extends Player {
+	public HumanPlayer() {
+		
+	}
+	
+	public BoardCell pickLocation(Set<BoardCell> targets) {
+		BoardCell board_cell = new BoardCell();
+		return board_cell;
+	}
+	
+	public void makeAccusation() {
+		
+	}
+	
+	public void createSuggestion() {
+		
+	}
+}
