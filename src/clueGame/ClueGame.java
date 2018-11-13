25c73be47fcd74138b82abccec3336054dd4ce0c
Carl Schader <cschader@factorearth.com>
2018-11-12 20:31:43 -0700

    Detective Notes Completed

--- src/clueGame/ClueGame.java
+++ src/clueGame/ClueGame.java
@@ -1,10 +1,16 @@
 package clueGame;
 
 import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import java.util.ArrayList;
 
+import javax.swing.JCheckBox;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.border.EtchedBorder;
+import javax.swing.border.TitledBorder;
 
 public class ClueGame extends JFrame {
 	private Board board;
@@ -45,6 +51,99 @@ public class ClueGame extends JFrame {
 	public String getUserName() {
 		return userName;
 	}
+	
+	private JPanel makeDetectiveCheckBox(CardType cardType) {
+		JPanel panel = new JPanel();
+//		panel.setSize(256,256);
+		panel.setLayout(new GridLayout(4,1));
+		String title;
+		ArrayList<String> list;
+		
+		switch (cardType) {
+		case PERSON:
+			title = "Person";
+			list = board.getPlayerNames();
+			break;
+		case ROOM:
+			title = "Room";
+			list = board.getRooms();
+			break;
+		case WEAPON:
+			title = "Weapon";
+			list = board.getWeapons();
+			break;
+		default:
+			title = "Error";
+			list = new ArrayList<String>();
+			break;
+		}
+		
+		panel.setBorder(new TitledBorder (new EtchedBorder(), title));
+		for (String card : list){
+		    JCheckBox box = new JCheckBox(card);
+		    panel.add(box);
+		}
+		
+		return panel;
+	}
+	
+	private JPanel makeDetectiveDropDown(CardType cardType) {
+		JPanel panel = new JPanel();
+//		panel.setSize(256,256);
+		panel.setLayout(new GridLayout(4,1));
+		
+		String title;
+		ArrayList<String> bufferList;
+		String[] list;
+		
+		switch (cardType) {
+		case PERSON:
+			title = "Person Guess";
+			bufferList = new ArrayList<String>(board.getPlayerNames());
+			bufferList.add(0, "Unknown");
+			list = bufferList.toArray(new String[bufferList.size()]);
+			break;
+		case ROOM:
+			title = "Room Guess";
+			bufferList = new ArrayList<String>(board.getRooms());
+			bufferList.add(0, "Unknown");
+			list = bufferList.toArray(new String[bufferList.size()]);
+			break;
+		case WEAPON:
+			title = "Weapon Guess";
+			bufferList = new ArrayList<String>(board.getWeapons());
+			bufferList.add(0, "Unknown");
+			list = bufferList.toArray(new String[bufferList.size()]);
+			break;
+		default:
+			title = "Error";
+			list = new String[0];
+			break;
+		}
+		
+		panel.setBorder(new TitledBorder (new EtchedBorder(), title));
+		JComboBox<String> dropDown = new JComboBox<String>(list);
+		panel.add(dropDown);
+		
+		return panel;
+	}
+	
+	private JPanel makeDetectiveNotes() {
+		JPanel panel = new JPanel();
+//		panel.setSize(512,1024);
+		panel.setLayout(new GridLayout(3,2));
+		
+		panel.add(makeDetectiveCheckBox(CardType.PERSON));
+		panel.add(makeDetectiveDropDown(CardType.PERSON));
+		
+		panel.add(makeDetectiveCheckBox(CardType.ROOM));
+		panel.add(makeDetectiveDropDown(CardType.ROOM));
+		
+		panel.add(makeDetectiveCheckBox(CardType.WEAPON));
+		panel.add(makeDetectiveDropDown(CardType.WEAPON));
+		
+		return panel;
+	}
 
 	public static void main(String[] args) {
 		ClueGame window = new ClueGame();
@@ -53,7 +152,7 @@ public class ClueGame extends JFrame {
 		window.setVisible(true);
 		
 		JOptionPane.showMessageDialog(window,"You are " + window.getUserName() + ". Press ok to continue", "Welcome to Clue", JOptionPane.INFORMATION_MESSAGE);
-
+		JOptionPane.showOptionDialog(null, window.makeDetectiveNotes(), "Detective Notes", JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, null, new Object[]{}, null);
 	}
 	
 	
